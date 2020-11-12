2c2a9be437d88f556f8d7d8c92b196388fb8b70f
zhuxianyong <zhuxianyong@eversec.cn>
2020-11-12 17:59:24 +0800

    静态文件由nginx托管

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,19 +10,19 @@ import { RoleEntity } from './role/role.entiry'
 import { FileModule } from './file/file.module'
 import { FileEntity } from './file/file.entity'
 import { AuthModule } from './auth/auth.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { ConfigModule } from '@nestjs/config'
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { ConfigModule } from '@nestjs/config'
 import { dbConst } from './constants'
 // import { APP_GUARD } from '@nestjs/core'
 // import { RolesGuard } from './shared/roles.guard'
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-      exclude: ['/api*']
-    }),
-    ConfigModule.forRoot(),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    //   exclude: ['/api*']
+    // }),
+    // ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: dbConst.host,
